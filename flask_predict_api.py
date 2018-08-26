6d91ced76fa67b980aae4e916ea4afb5129f52a4
Brett <34327248+brf217@users.noreply.github.com>
2018-08-26 16:14:57 -0600

    Add files via upload

---
+++ flask_predict_api.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 26 14:16:21 2018
+
+@author: feebr01
+"""
+
+import pickle
+from flask import Flask, request
+import numpy as np
+import pandas as pd
+from flasgger import Swagger
+
+
+#open pickle file and make sure to tell it is a binary 'rb' read
+with open ('/Users/feebr01/Documents/p_docker/rf.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+    
+
+app = Flask(__name__)
+swagger = Swagger(app)  #make ui look nice
+
+@app.route('/predict')
+def predict_iris():
+    '''Example enpoint returning a prediction of iris
+    ---
+    parameters:
+        - name: s_length
+          in: query
+          type: number
+          required: true
+    
+        - name: s_width
+          in: query
+          type: number
+          required: true    
+     
+        - name: p_length
+          in: query
+          type: number
+          required: true    
+
+        - name: p_width
+          in: query
+          type: number
+          required: true
+    responses:
+        '200':
+            description: "text"
+    '''   
+    s_length = request.args.get('s_length')
+    s_width = request.args.get('s_width')
+    p_length = request.args.get('p_length')
+    p_width = request.args.get('p_width')
+    
+    
+    prediction = model.predict(np.array([[s_length,s_width,
+                                          p_length,p_width]]))
+    
+    return str(prediction)
+
+
+@app.route('/predict_file', methods = ['POST'])
+def predict_iris_file():
+    '''Example file enpoint returning a prediction of iris
+    ---
+    parameters:
+        - name: input_file
+          in: formData
+          type: file
+          required: true
+    responses:
+        '200':
+            description: "text"
+    '''
+    input_data = pd.read_csv(request.files.get('input_file'), header=None)
+    prediction = model.predict(input_data)
+    return str(list(prediction))
+
+
+if __name__ == '__main__':
+    app.run()
+    
+    
+    
+    
+'''http://localhost:5000/apidocs/    - how to see flask api ui'''
+    
+    
+    
+    
---
+++ flask_predict_train.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 26 10:59:09 2018
+
+@author: feebr01
+"""
+
+ 
+#RF Classifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+#load data
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+#split data
+X_train, X_test, y_train, y_test = train_test_split(X,y, 
+    random_state = 42, test_size = 0.5)
+
+
+# model
+clf = RandomForestClassifier(n_estimators = 10)
+
+#train model
+clf.fit(X_train, y_train)
+
+
+#predict
+predicted = clf.predict(X_test)
+
+
+#check accuracy
+print(accuracy_score(predicted, y_test))
+
+
+#pickle the file for later use
+import pickle
+
+with open('/Users/feebr01/Documents/p_docker/rf.pkl', 'wb') as model_pkl:
+    pickle.dump(clf, model_pkl)
+    
